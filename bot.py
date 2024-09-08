2dfee35782261a4c6630e540faf24885b976edcd
BelyaevAlex <belyaevalex2007@gmail.com>
2024-09-08 14:24:59 +0200

    Added model

---
+++ bot.py
@@ -0,0 +1,33 @@
+import logging
+import asyncio
+from aiogram import Bot, Dispatcher, types
+from model import Model
+
+api_token = '7428827735:AAGbsVtSefZP5tvOd4-l5qo7UoXsTR2cPGg'
+logging.basicConfig(level=logging.INFO)
+
+
+class TelegBot:
+    def __init__(self, token):
+        pdf_path = 'content/język_polski3.pdf'
+        self.bot = Bot(token=token)
+        self.dp = Dispatcher()
+        self.dp.message.register(self.handle_m)
+
+        self.model = Model(pdf_path)
+
+
+    async def start(self):
+        logging.info("Uruchomiony")
+
+        await self.bot.delete_webhook(drop_pending_updates=True)
+        await self.dp.start_polling(self.bot)
+
+    async def handle_m(self, message: types.Message):
+        question = message.text
+        logging.info(f"Otrzymano: {question}.")
+        answer = self.model.ask(question)
+        await message.answer(answer)
+
+bot = TelegBot(api_token)
+asyncio.run(bot.start())
