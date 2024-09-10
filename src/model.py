3f8885b7936f69650fa45708dffd02e4bb1a3eb1
BelyaevAlex <belyaevalex2007@gmail.com>
2024-09-10 21:01:05 +0200

    Added readme.md

--- src/model.py
+++ src/model.py
@@ -21,11 +21,9 @@ Pytanie: [{}]""")
         with pdfplumber.open(self.pdf_path) as pdf:
             for page in pdf.pages:
                 text += page.extract_text() + "\n"
-        print(text)
         document = text.split('---')
         for i in text.split('___'):
             document.append(i)
-        print(len(document))
         return document
 
 
